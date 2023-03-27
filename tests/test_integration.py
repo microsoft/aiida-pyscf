a45dd743e5b7cec2ac694f3dca2fa18c3e384490
Sebastiaan Huber <mail@sphuber.net>
2023-03-27 12:13:28 +0200

    Tests: Add integration tests for `PyscfCalculation` (#2)
    The tests launch a `PyscfCalculation` for an H2O molecule. The first
    performs just a default mean field calculation, whereas the second
    performs a geometry optimization. Unlike the unit tests, this tests the
    full cycle of launching a PySCF calculation on the localhost using
    `PyscfCalculation` and having the results parsed by `PyscfParser`.
    
    The parsed `forces` are checked using `num_regression` instead of
    `data_regression`. The reason is that the water molecule geometry is
    close to optimal and so the forces are close to zero. The parsed values
    typically have small noise in the float values, causing `data_regression`
    to fail. The `num_regression` fixture uses the `numpy.is_close` assertion
    that accounts for these minor fluctuations and ignores them.
    
    The `pyscf` dependency is added, together with the `geomopt` extra which
    installs the dependencies to run geometry optimizations.

---
+++ tests/test_integration.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""Module with integration tests."""
+from aiida import engine, orm
+import numpy
+
+
+def test_pyscf_base_mean_field(aiida_local_code_factory, generate_structure, data_regression, num_regression):
+    """Test running a default mean field ``PyscfCalculation`` job."""
+    code = aiida_local_code_factory('pyscf.base', 'python')
+    builder = code.get_builder()
+    builder.structure = generate_structure()
+
+    results, node = engine.run_get_node(builder)
+    assert node.is_finished_ok
+
+    parameters = results['parameters'].get_dict()
+
+    # Timings differ from run to run, so we just check the expected keys exist and are floats.
+    timings = parameters.pop('timings')
+    for key in ('total', 'mean_field'):
+        assert key in timings
+        assert isinstance(timings[key], float)
+
+    # The structure is a water molecule with ideal structure so forces are close to zero. They are compared using the
+    # ``num_regression`` fixture to account for negligible float value differences.
+    forces = parameters.pop('forces')
+    num_regression.check({'forces': numpy.array(forces).flatten()}, default_tolerance={'atol': 1e-4, 'rtol': 1e-18})
+
+    data_regression.check(parameters)
+
+
+def test_pyscf_base_geometry_optimization(
+    aiida_local_code_factory, generate_structure, data_regression, num_regression
+):
+    """Test running a geometry optimization ``PyscfCalculation`` job."""
+    code = aiida_local_code_factory('pyscf.base', 'python')
+    builder = code.get_builder()
+    builder.structure = generate_structure()
+    builder.parameters = orm.Dict({
+        'optimizer': {
+            'solver': 'geomeTRIC',
+        },
+    })
+    results, node = engine.run_get_node(builder)
+    assert node.is_finished_ok
+
+    parameters = results['parameters'].get_dict()
+
+    # Timings differ from run to run, so we just check the expected keys exist and are floats.
+    timings = parameters.pop('timings')
+    for key in ('total', 'mean_field', 'optimizer'):
+        assert key in timings
+        assert isinstance(timings[key], float)
+
+    # The structure is a water molecule with ideal structure so forces are close to zero. They are compared using the
+    # ``num_regression`` fixture to account for negligible float value differences. The same goes for the optimized
+    # coordinates which can have small fluctuations due to randomness in the relaxation path.
+    forces = parameters.pop('forces')
+    optimized_coordinates = parameters.pop('optimized_coordinates')
+    num_regression.check(
+        {
+            'forces': numpy.array(forces).flatten(),
+            'optimized_coordinates': numpy.array(optimized_coordinates).flatten()
+        },
+        default_tolerance={
+            'atol': 1e-4,
+            'rtol': 1e-18
+        },
+    )
+    data_regression.check(parameters)
