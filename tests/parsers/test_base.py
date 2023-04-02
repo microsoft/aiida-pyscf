b36f62d1abc76729c8de2c1eb4007229b73325a5
Sebastiaan Huber <mail@sphuber.net>
2023-04-02 15:14:41 +0200

    `PyscfParser`: Add unit tests for parsing of FCIDUMP and CUBE files

--- tests/parsers/test_base.py
+++ tests/parsers/test_base.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Tests for the :mod:`aiida_pyscf.parsers.base` module."""
 # pylint: disable=redefined-outer-name
+from aiida.orm import SinglefileData
+
 from aiida_pyscf.calculations.base import PyscfCalculation
 
 
@@ -36,3 +38,29 @@ def test_failed_missing_result(generate_calc_job_node, generate_parser):
 
     assert calcfunction.is_failed
     assert calcfunction.exit_status == PyscfCalculation.exit_codes.ERROR_OUTPUT_RESULTS_MISSING.status
+
+
+def test_cubegen(generate_calc_job_node, generate_parser, generate_structure):
+    """Test parsing the outputs of a job that computed CUBE files."""
+    inputs = {'structure': generate_structure('N2')}
+    node, tmp_path = generate_calc_job_node('pyscf.base', 'cubegen', inputs, retrieve_temporary_list=['*.cube'])
+    parser = generate_parser('pyscf.base')
+    results, calcfunction = parser.parse_from_node(node, retrieved_temporary_folder=tmp_path, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    assert 'cubegen' in results
+    assert all(isinstance(node, SinglefileData) for node in results['cubegen'].values())
+
+
+def test_fcidump(generate_calc_job_node, generate_parser, generate_structure):
+    """Test parsing the outputs of a job that computed FCIDUMP files."""
+    inputs = {'structure': generate_structure('N2')}
+    node, tmp_path = generate_calc_job_node('pyscf.base', 'fcidump', inputs, retrieve_temporary_list=['*.fcidump'])
+    parser = generate_parser('pyscf.base')
+    results, calcfunction = parser.parse_from_node(node, retrieved_temporary_folder=tmp_path, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    assert 'fcidump' in results
+    assert all(isinstance(node, SinglefileData) for node in results['fcidump'].values())
